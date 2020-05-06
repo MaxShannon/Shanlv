7e3383a4df568bc10160fccb39d6fbcad51f7348
Max Shannon <1525411339@qq.com>
2020-05-06 17:47:54 +0800

    ef

--- Shanlv.DotNetCore/Startup.cs
+++ Shanlv.DotNetCore/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shanlv.EfDbModels;
 
 namespace Shanlv.DotNetCore
 {
@@ -20,13 +17,20 @@ namespace Shanlv.DotNetCore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+                //services.AddSingleton<IRepository<Student>, InMemoryRepository>();
+            
+
+            services.AddDbContext<ShanlvDbContext>(a =>
+            {
+                a.EnableSensitiveDataLogging();
+                a.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=EFCoreDemo; Trusted_connection=true;");
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +40,6 @@ namespace Shanlv.DotNetCore
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
--- Shanlv.EfDbModels/ShanlvDbContext.cs
+++ Shanlv.EfDbModels/ShanlvDbContext.cs
@@ -5,14 +5,14 @@ namespace Shanlv.EfDbModels
 {
     public class ShanlvDbContext : DbContext
     {
-        public ShanlvDbContext() //: base("con")
+        public ShanlvDbContext(DbContextOptions<ShanlvDbContext> options) : base(options)
         {
             //Database.SetInitializer<BlogContext>(null);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = "server=.;database=BlogSystem;uid=sa;pwd=sa";
+            var connectionString = "server=10.28.7.12;database=Shanlv;uid=sa;pwd=Admin123456";
             optionsBuilder.UseSqlServer(connectionString);
         }
 
@@ -28,6 +28,8 @@ namespace Shanlv.EfDbModels
 
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Cargo> Cargoes { get; set; }
+        public DbSet<Shipment> Shipments { get; set; }
         //public DbSet<Article> Articles { get; set; }
         //public DbSet<ArticleToCategory> ArticleToCategories { get; set; }
         //public DbSet<Comment> Comments { get; set; }
